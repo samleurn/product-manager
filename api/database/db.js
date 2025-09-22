02eca22361c27e6de545f3112c29e53ad9f3dec5
Samleurn <samleudev@gmail.com>
2025-09-22 20:28:35 -0300

    Criando connecção com o banco de dados

---
+++ api/database/db.js
@@ -0,0 +1,13 @@
+import knex from 'knex'
+
+const db = knex({
+  client: process.env.DB_CLIENT || 'pg',
+  connection: {
+    host: process.env.DB_HOST || '',
+    user: process.env.DB_USER || '',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || '',
+  },
+})
+
+export { db }
